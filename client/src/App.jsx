f794328a6b422e967a1c2a71fe947b7fd6622222
Tanmaykushwaha01 <tanmaykushwaha0101@gmail.com>
2024-05-01 10:26:39 +0530

    added complete frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,71 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
 import axios from "axios";
-import { baseURL } from './utils/constant';
+import { baseURL } from "./utils/constant";
+import Popup from "./components/Popup";
 
 const App = () => {
- const [toDos, setToDos] = useState([]); 
+  const [toDos, setToDos] = useState([]);
   const [input, setInput] = useState("");
   const [updateUI, setUpdateUI] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupContent, setPopupContent] = useState({});
 
- useEffect(() => {
-  axios.get(`${baseURL}/get`)
-  .then((res) => setToDos(res.data))
-  .catch((err) => console.log(err));
-}, [updateUI]);
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/get`)
+      .then((res) => setToDos(res.data))
+      .catch((err) => console.log(err));
+  }, [updateUI]);
 
- const saveToDo = () => {
+  const saveToDo = () => {
     axios
       .post(`${baseURL}/save`, { toDo: input })
       .then((res) => {
         console.log(res.data);
-        setUpdateUI((prevState) => !prevState);  
+        setUpdateUI((prevState) => !prevState);
         setInput("");
       })
       .catch((err) => console.log(err));
   };
 
-return (
+  return (
     <main>
       <div className="container">
         <h1 className="title">ToDo App</h1>
 
-        <div className='input_holder'>
-            <input value={input} onChange={(e) => setInput(e.target.value)} type="text"placeholder="Add a Todo..." />
-            <button  onClick={saveToDo}>Add</button>
+        <div className="input_holder">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Add a Todo..."
+          />
+          <button onClick={saveToDo}>Add</button>
         </div>
 
-        <div className='list'>
-          {console.log(toDos)}
-          {toDos && toDos.map(el => <ToDo key={el._id} text={el.todo} />)}
-         
+        <div className="list">
+          {/* {console.log(toDos)} */}
+          {toDos.map((el) => (
+            <ToDo key={el._id} 
+            text={el.toDo} 
+            id={el._id}
+            setUpdateUI={setUpdateUI}
+            setShowPopup={setShowPopup}
+            setPopupContent={setPopupContent}
+            />
+          ))}
         </div>
-    </div>
+      </div>
+      {showPopup && (
+        <Popup
+          setShowPopup={setShowPopup}
+          popupContent={popupContent}
+          setUpdateUI={setUpdateUI}
+        />
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
